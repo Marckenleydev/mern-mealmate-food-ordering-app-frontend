13a564ffbf22389dafaa8bac9377e32843323046
Marckenley Dorsainvil <marckenlygbeau@gmail.com>
2025-01-29 19:27:15 +0400

    modified the backend Url API

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { Link } from 'react-router'
 import MobileNav from './MobileNav'
 import MainNav from './MainNav'
@@ -8,7 +8,7 @@ const Header = () => {
     <div className='border-b-2 border-b-orange-500 py-6'>
         <div className="container flex mx-auto justify-between items-center">
             <Link  to="/" className='text-xl font-bold tracking-tight text-orange-500'>
-            MERNEats</Link>
+            MEALMATEFood</Link>
             <div className='md:hidden bg-white'>
                 <MobileNav/>
             </div>
